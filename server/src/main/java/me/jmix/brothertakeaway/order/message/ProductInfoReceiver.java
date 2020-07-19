087f2573d393ab7ea6c25318b5ed473e6446c7b2
JellyfishMIX <2273516339@qq.com>
2020-07-19 10:00:19 +0800

    :construction: Updated with spring-cloud-config

--- server/src/main/java/me/jmix/brothertakeaway/order/message/ProductInfoReceiver.java
+++ server/src/main/java/me/jmix/brothertakeaway/order/message/ProductInfoReceiver.java
@@ -1,11 +1,16 @@
 package me.jmix.brothertakeaway.order.message;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.slf4j.Slf4j;
 import me.jmix.brothertakeaway.order.utils.JsonUtil;
 import me.jmix.brothertakeaway.product.entity.ProductInfo;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * @author JellyfishMIX
@@ -14,12 +19,22 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class ProductInfoReceiver {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    private static final String PRODUCT_STOCK_TEMPLATE = "product-stock-%s";
 
     @RabbitListener(queuesToDeclare = @Queue("productInfo"))
     public void process(String message) {
         // message => ProductInfo
-        ProductInfo productInfo = (ProductInfo) JsonUtil.fromJson(message, ProductInfo.class);
-        log.info("从队列【{}】接收到消息: {}", "productInfo", productInfo);
-    }
+        List<ProductInfo> productInfoList = (List<ProductInfo>) JsonUtil.fromJson(message, new TypeReference<List<ProductInfo>>() {});
+        log.info("从队列【{}】接收到消息: {}", "productInfoList", productInfoList);
 
+        // 存到redis中
+        for (ProductInfo productInfo : productInfoList) {
+            String key = String.format(PRODUCT_STOCK_TEMPLATE, String.valueOf(productInfo.getProductId()));
+            String value = String.valueOf(productInfo.getProductStock());
+            stringRedisTemplate.opsForValue().set(key, value);
+        }
+    }
 }
